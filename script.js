c3b3d6687548ea9b9a9ff1a06899aff0fb6a9e12
totekits <tote.kits@gmail.com>
2023-09-27 19:33:53 +0700

    Finish the script

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const gridContainer = document.getElementById('grid-container');
 const button = document.getElementById('resize');
 
-const rows = 16;
-const cols = 16;
+const maxGridSize = 100;
 
-for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        gridContainer.append(square);
+let gridSize = 16;
+
+function createGrid() {
+    gridContainer.innerHTML = '';
+
+    const squareSize = 640 / gridSize;
+
+    gridSize = Math.min(gridSize, maxGridSize);
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        gridContainer.appendChild(square);
     }
 }
 
@@ -20,7 +29,16 @@ function handleHover(event) {
 
 function promptForNewSize(event) {
     const newSize = prompt('Enter the number of squares per side for the new grid:')
+
+    if (!isNaN(newSize) && newSize > 0 && newSize <= 100) {
+        gridSize = parseInt(newSize);
+        createGrid();
+    } else {
+        alert('Please enter a positive number between 1-100')
+    }
 }
 
 gridContainer.addEventListener('mouseover', handleHover);
 button.addEventListener('click', promptForNewSize);
+
+createGrid();
